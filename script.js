9e30b7dcfe214812f4649f5c5cadefc081306dd1
vasanthfeb <vasanthfeb18@gmail.com>
2025-07-23 20:20:46 +0530

    about

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-$(document).ready(function() {
-        $('.testimonial-slider').slick({
-            autoplay: false,
-            autoplaySpeed: 1000,
-            speed: 600,
-            draggable: false,
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            arrows: true,
-            dots: true,
-            responsive: [
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                  }
-                },
-                {
-                    breakpoint: 575,
-                    settings: {
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                    }
-                }
-            ]
-        });
-    }); 
+ $(document).ready(function () {
+	$('.slideset').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 850,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		}]
+	});
+});
